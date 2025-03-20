f02a8482dbec54ca841daa9cb37d33d2a0d03c65
RoyGV <gomezvalencia.rogelioisaac.proga@gmail.com>
2025-03-20 09:53:15 -0600

    Changes to html and js,

--- main.js
+++ main.js
@@ -1,54 +1,69 @@
 function getSelectionText() {
-    let text = "";
+  let text = "";
 
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
-    }
+  if (window.getSelection) {
+    text = window.getSelection().toString();
+  } else if (document.selection && document.selection.type != "Control") {
+    text = document.selection.createRange().text;
+  }
 
-    return text;
+  return text;
 }
 
 let currentMode = "Normal";
 
 window.onload = function () {
-    const btnNormalTxt = document.getElementById('btnNormal');
-    btnNormalTxt.onclick = function () {
-        normalText();
-        changeText();
-    };
-}
+  const btnItalicTxt = document.getElementById("btnBold");
+  const btnBoldTxt = document.getElementById("btnItalic");
 
-function changeText()
-{
-    const text = getSelectionText();
-    const editor = document.getElementById('editor');
-    const editorTxt = editor.value;
-    if (editorTxt.includes(text))
-    {
-        editor.value=`${editorTxt.replace(text,"Hola")}`;
-        alert("A "+ editorTxt + 'Hola: '+text);
+  addEventListener("keypress", (event) => {
+    const input = String.fromCharCode(event.keyCode);
+    document.getElementById("editor").innerHTML =
+      document.getElementById("editor").innerHTML + input;
+  });
+
+  btnItalicTxt.onclick = function () {
+    boldText();
+    changeText();
+  };
+
+  btnBoldTxt.onclick = function () {
+    italicText();
+    changeText();
+  };
+};
+
+function changeText() {
+  const text = getSelectionText();
+  const editor = document.getElementById("editor");
+  const editorTxt = editor.innerHTML;
+
+  if (editorTxt.includes(text)) {
+    switch (currentMode) {
+      case "Normal":
+        editor.innerHTML = editorTxt.replace(text, `<p>${text}</p>`);
+        break;
+      case "Bold":
+        editor.innerHTML = editorTxt.replace(text, `<b>${text}</b>`);
+        break;
+      case "Italic":
+        editor.innerHTML = editorTxt.replace(text, `<i>${text}</i>`);
+
+        break;
     }
-}
-
-function normalText()
-{
-    currentMode = "Normal";
-}
-
-function boldText()
-{
-    currentMode = "Bold";
     
+  }
 }
 
-function italicText()
-{
-    currentMode = "Italic";
+function normalText() {
+  currentMode = "Normal";
 }
 
-function normalText()
-{
-    currentMode = "Normal";
+function boldText() {
+  currentMode = "Bold";
 }
+
+function italicText() {
+  currentMode = "Italic";
+}
+
